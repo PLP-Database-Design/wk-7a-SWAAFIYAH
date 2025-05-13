c12cf0615b35fc125606bf30a17b56fe4e56e6b4
Sophia Matano Salim <133153481+SWAAFIYAH@users.noreply.github.com>
2025-05-13 13:29:08 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -48,3 +48,48 @@ FROM SplitProducts;
 
 
 --Question2
+USE mysqlTest;
+
+CREATE TABLE OrderDetails (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100),
+    Quantity INT
+);
+
+
+INSERT INTO OrderDetails (OrderID, CustomerName, Product, Quantity)
+VALUES
+(101, 'John Doe', 'Laptop', 2),
+(101, 'John Doe', 'Mouse', 1),
+(102, 'Jane Smith', 'Tablet', 3),
+(102, 'Jane Smith', 'Keyboard', 1),
+(102, 'Jane Smith', 'Mouse', 2),
+(103, 'Emily Clark', 'Phone', 1);
+
+
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT DISTINCT OrderID, CustomerName
+FROM OrderDetails;
+
+
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
+SELECT * FROM Orders;
+SELECT * FROM OrderItems;
+
+
+
